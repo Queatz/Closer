7e31c434f68c74d1ad6cc77c5d1879be5d1378ec
Jacob Ferrero <jacobaferrero@gmail.com>
2018-06-22 22:35:14 -0500

    Fix crash when location is unavailable

--- app/src/main/java/closer/vlllage/com/closer/handler/helpers/OutboundHandler.java
+++ app/src/main/java/closer/vlllage/com/closer/handler/helpers/OutboundHandler.java
@@ -9,6 +9,11 @@ import closer.vlllage.com.closer.pool.PoolMember;
 
 public class OutboundHandler extends PoolMember {
     public void openDirections(LatLng latLng) {
+        if (latLng == null) {
+            $(DefaultAlerts.class).thatDidntWork();
+            return;
+        }
+
         Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
                 Uri.parse("https://www.google.com/maps/dir/?api=1&destination=" + latLng.latitude + "," + latLng.longitude));
         $(ActivityHandler.class).getActivity().startActivity(intent);
