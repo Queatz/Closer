fb979ebefa86a770d77410cae929abd8e8645dc9
Jacob Ferrero <jacobaferrero@gmail.com>
2018-09-06 22:48:15 -0500

    Add BubbleProxy

---
+++ app/src/main/java/closer/vlllage/com/closer/handler/bubble/BubbleProxy.java
@@ -0,0 +1,17 @@
+package closer.vlllage.com.closer.handler.bubble;
+
+import closer.vlllage.com.closer.pool.PoolMember;
+
+public class BubbleProxy extends PoolMember {
+    public void add(MapBubble mapBubble) {
+
+    }
+
+    public void move(MapBubble mapBubble) {
+
+    }
+
+    public void remove(MapBubble mapBubble) {
+
+    }
+}
