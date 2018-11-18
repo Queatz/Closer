b301464a8d169b18731b44a84598bcd6faa0011b
Jacob Ferrero <jacobaferrero@gmail.com>
2018-11-18 11:35:15 -0600

    Clean up GroupMessagesAdapter.java

--- app/src/main/java/closer/vlllage/com/closer/handler/group/GroupMessagesAdapter.java
+++ app/src/main/java/closer/vlllage/com/closer/handler/group/GroupMessagesAdapter.java
@@ -2,8 +2,6 @@ package closer.vlllage.com.closer.handler.group;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.text.format.DateUtils;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,20 +10,14 @@ import android.widget.TextView;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import closer.vlllage.com.closer.R;
-import closer.vlllage.com.closer.handler.event.EventDetailsHandler;
-import closer.vlllage.com.closer.handler.helpers.ApplicationHandler;
 import closer.vlllage.com.closer.handler.helpers.JsonHandler;
 import closer.vlllage.com.closer.handler.helpers.ResourcesHandler;
 import closer.vlllage.com.closer.handler.helpers.Val;
-import closer.vlllage.com.closer.handler.phone.NameHandler;
-import closer.vlllage.com.closer.handler.phone.PhoneMessagesHandler;
 import closer.vlllage.com.closer.pool.PoolMember;
 import closer.vlllage.com.closer.pool.PoolRecyclerAdapter;
 import closer.vlllage.com.closer.store.StoreHandler;
@@ -33,13 +25,7 @@ import closer.vlllage.com.closer.store.models.Event;
 import closer.vlllage.com.closer.store.models.Group;
 import closer.vlllage.com.closer.store.models.GroupMessage;
 import closer.vlllage.com.closer.store.models.Group_;
-import closer.vlllage.com.closer.store.models.Phone;
-import closer.vlllage.com.closer.store.models.Phone_;
 import closer.vlllage.com.closer.store.models.Suggestion;
-import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
-
-import static android.text.format.DateUtils.MINUTE_IN_MILLIS;
-import static android.text.format.DateUtils.WEEK_IN_MILLIS;
 
 public class GroupMessagesAdapter extends PoolRecyclerAdapter<GroupMessagesAdapter.GroupMessageViewHolder> {
 
@@ -110,212 +96,25 @@ public class GroupMessagesAdapter extends PoolRecyclerAdapter<GroupMessagesAdapt
             try {
                 JsonObject jsonObject = $(JsonHandler.class).from(groupMessage.getAttachment(), JsonObject.class);
                 if (jsonObject.has("action")) {
-                    Phone phone = getPhone(groupMessage.getFrom());
-                    String contactName = $(NameHandler.class).getName(phone);
-
-                    JsonObject action = jsonObject.get("action").getAsJsonObject();
-                    holder.name.setVisibility(View.VISIBLE);
-                    holder.name.setText(contactName + " " + action.get("intent").getAsString());
-                    holder.time.setVisibility(View.VISIBLE);
-                    holder.time.setText(getTimeString(groupMessage.getTime()));
-
-                    String comment = action.get("comment").getAsString();
-                    if ($(Val.class).isEmpty(comment)) {
-                        holder.message.setVisibility(View.GONE);
-                    } else {
-                        holder.message.setVisibility(View.VISIBLE);
-                        holder.message.setGravity(Gravity.START);
-                        holder.message.setAlpha(1f);
-                        holder.message.setText(action.get("comment").getAsString());
-                    }
-                    holder.action.setVisibility(View.GONE);
-
-                    holder.action.setVisibility(View.VISIBLE);
-                    holder.action.setText($(ResourcesHandler.class).getResources().getString(R.string.reply));
-                    holder.action.setOnClickListener(view -> {
-                        $(PhoneMessagesHandler.class).openMessagesWithPhone(phone.getId(), contactName, comment);
-                    });
+                    $(MessageDisplay.class).displayAction(holder, jsonObject, groupMessage);
                 } else if (jsonObject.has("message")) {
-                    holder.name.setVisibility(View.GONE);
-                    holder.time.setVisibility(View.GONE);
-                    holder.message.setVisibility(View.VISIBLE);
-                    holder.message.setGravity(Gravity.CENTER_HORIZONTAL);
-                    holder.message.setText(jsonObject.get("message").getAsString());
-                    holder.message.setAlpha(.5f);
-                    holder.itemView.setOnClickListener(null);
-                    holder.action.setVisibility(View.GONE);
+                    $(MessageDisplay.class).displayMessage(holder, jsonObject, groupMessage);
                 } else if (jsonObject.has("event")) {
-                    final Event event = $(JsonHandler.class).from(jsonObject.get("event"), Event.class);
-
-                    holder.name.setVisibility(View.VISIBLE);
-                    holder.time.setVisibility(View.VISIBLE);
-                    holder.time.setText(getTimeString(groupMessage.getTime()));
-
-                    Phone phone = getPhone(groupMessage.getFrom());
-
-                    String contactName = $(NameHandler.class).getName(phone);
-
-                    holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.phone_shared_an_event, contactName));
-
-                    holder.message.setVisibility(View.VISIBLE);
-                    holder.message.setGravity(Gravity.START);
-                    holder.message.setAlpha(1f);
-                    holder.message.setText(
-                            (event.getName() == null ? $(ResourcesHandler.class).getResources().getString(R.string.unknown) : event.getName()) +
-                                    "\n" +
-                                    $(EventDetailsHandler.class).formatEventDetails(event));
-
-                    holder.action.setVisibility(View.VISIBLE);
-                    holder.action.setText($(ResourcesHandler.class).getResources().getString(R.string.show_on_map));
-                    holder.action.setOnClickListener(view -> {
-                        if (onEventClickListener != null) {
-                            onEventClickListener.onEventClick(event);
-                        }
-                    });
+                    $(MessageDisplay.class).displayEvent(holder, jsonObject, groupMessage, onEventClickListener);
                 } else if (jsonObject.has("suggestion")) {
-                    final Suggestion suggestion = $(JsonHandler.class).from(jsonObject.get("suggestion"), Suggestion.class);
-
-                    boolean suggestionHasNoName = suggestion == null || suggestion.getName() == null || suggestion.getName().isEmpty();
-
-                    holder.name.setVisibility(View.VISIBLE);
-                    holder.time.setVisibility(View.VISIBLE);
-                    holder.time.setText(getTimeString(groupMessage.getTime()));
-
-                    Phone phone = getPhone(groupMessage.getFrom());
-
-                    String contactName = $(NameHandler.class).getName(phone);
-
-                    if (suggestionHasNoName) {
-                        holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.phone_shared_a_location, contactName));
-                    } else {
-                        holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.phone_shared_a_suggestion, contactName));
-                    }
-
-                    if (suggestionHasNoName) {
-                        holder.message.setVisibility(View.GONE);
-                    } else {
-                        holder.message.setVisibility(View.VISIBLE);
-                        holder.message.setGravity(Gravity.START);
-                        holder.message.setAlpha(1f);
-                        holder.message.setText(suggestion.getName());
-                    }
-
-                    holder.action.setVisibility(View.VISIBLE);
-                    holder.action.setText($(ResourcesHandler.class).getResources().getString(R.string.show_on_map));
-                    holder.action.setOnClickListener(view -> {
-                        if (onSuggestionClickListener != null) {
-                            onSuggestionClickListener.onSuggestionClick(suggestion);
-                        }
-                    });
+                    $(MessageDisplay.class).displaySuggestion(holder, jsonObject, groupMessage, onSuggestionClickListener);
                 } else if (jsonObject.has("photo")) {
-                    Phone phone = getPhone(groupMessage.getFrom());
-                    String contactName = $(NameHandler.class).getName(phone);
-
-                    final String photo = jsonObject.get("photo").getAsString() + "?s=500";
-                    holder.name.setVisibility(View.VISIBLE);
-                    holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.phone_shared_a_photo, contactName));
-                    holder.time.setVisibility(View.VISIBLE);
-                    holder.time.setText(getTimeString(groupMessage.getTime()));
-                    holder.message.setVisibility(View.GONE);
-                    holder.action.setVisibility(View.GONE);// or Share / save photo?
-                    holder.photo.setVisibility(View.VISIBLE);
-                    holder.photo.setOnClickListener(view -> $(PhotoActivityTransitionHandler.class).show(view, photo));
-                    Picasso.get().cancelRequest(holder.photo);
-                    Picasso.get().load(photo).transform(new RoundedCornersTransformation($(ResourcesHandler.class).getResources().getDimensionPixelSize(R.dimen.imageCorners), 0)).into(holder.photo);
+                    $(MessageDisplay.class).displayPhoto(holder, jsonObject, groupMessage);
                 } else {
-                    holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.unknown));
-                    holder.message.setText("");
-                    holder.time.setVisibility(View.VISIBLE);
-                    holder.time.setText(getTimeString(groupMessage.getTime()));
+                    $(MessageDisplay.class).displayFallback(holder, groupMessage);
                 }
-
-                return;
             } catch (JsonSyntaxException e) {
+                $(MessageDisplay.class).displayFallback(holder, groupMessage);
                 e.printStackTrace();
             }
-        }
-
-        int pairIndex = position + (isReversed ? -1 : 1);
-        boolean validIndex = pairIndex >= 0 && pairIndex < getItemCount();
-
-        boolean previousMessageIsSameContact = validIndex &&
-                groupMessages.get(pairIndex).getAttachment() == null &&
-                groupMessages.get(pairIndex).getFrom() != null &&
-                groupMessages.get(pairIndex).getFrom().equals(groupMessage.getFrom());
-
-        boolean previousMessageIsSameGroup = validIndex &&
-                groupMessages.get(pairIndex).getTo() != null &&
-                groupMessages.get(pairIndex).getTo().equals(groupMessage.getTo());
-
-        boolean previousMessageIsSameTime = validIndex &&
-                groupMessages.get(pairIndex).getAttachment() == null &&
-                getTimeString(groupMessages.get(pairIndex).getTime()).equals(getTimeString(groupMessage.getTime()));
-
-        holder.action.setVisibility(View.GONE);
-        holder.name.setVisibility(previousMessageIsSameContact && previousMessageIsSameGroup ? View.GONE : View.VISIBLE);
-        holder.message.setVisibility(View.VISIBLE);
-        holder.message.setGravity(Gravity.START);
-
-        if (previousMessageIsSameTime) {
-            holder.time.setVisibility(View.GONE);
         } else {
-            holder.time.setVisibility(View.VISIBLE);
-            holder.time.setText(getTimeString(groupMessage.getTime()));
+            $(MessageDisplay.class).displayGroupMessage(holder, isReversed, groupMessage, groupMessages, position, getItemCount());
         }
-
-        Phone phone = null;
-
-        if (groupMessage.getFrom() != null) {
-            phone = getPhone(groupMessage.getFrom());
-        }
-
-        holder.name.setText($(NameHandler.class).getName(phone));
-        holder.message.setText(groupMessage.getText());
-        holder.message.setAlpha(groupMessage.isLocalOnly() ? .5f : 1f);
-    }
-
-    private Phone getPhone(String phoneId) {
-        if (phoneId == null) {
-            return null;
-        }
-
-        return $(StoreHandler.class).getStore().box(Phone.class).query()
-                .equal(Phone_.id, phoneId)
-                .build()
-                .findFirst();
-    }
-
-    private Group getGroup(String groupId) {
-        if (groupId == null) {
-            return null;
-        }
-
-        return $(StoreHandler.class).getStore().box(Group.class).query()
-                .equal(Group_.id, groupId)
-                .build()
-                .findFirst();
-    }
-
-    private String getTimeString(Date date) {
-        if (date == null) {
-            return "";
-        }
-
-        if (new Date().getTime() - date.getTime() < 5 * MINUTE_IN_MILLIS) {
-            return "";
-        }
-
-        if (DateUtils.isToday(date.getTime())) {
-            return DateUtils.getRelativeTimeSpanString(date.getTime()).toString();
-        }
-
-        return DateUtils.getRelativeDateTimeString(
-                $(ApplicationHandler.class).getApp(),
-                date.getTime(),
-                MINUTE_IN_MILLIS,
-                WEEK_IN_MILLIS,
-                0
-        ).toString();
     }
 
     @Override
@@ -356,6 +155,17 @@ public class GroupMessagesAdapter extends PoolRecyclerAdapter<GroupMessagesAdapt
             group = itemView.findViewById(R.id.group);
             photo = itemView.findViewById(R.id.photo);
         }
+    }
+
+    private Group getGroup(String groupId) {
+        if (groupId == null) {
+            return null;
+        }
+
+        return $(StoreHandler.class).getStore().box(Group.class).query()
+                .equal(Group_.id, groupId)
+                .build()
+                .findFirst();
     }
 
     public interface OnMessageClickListener {
---
+++ app/src/main/java/closer/vlllage/com/closer/handler/group/MessageDisplay.java
@@ -0,0 +1,235 @@
+package closer.vlllage.com.closer.handler.group;
+
+import android.text.format.DateUtils;
+import android.view.Gravity;
+import android.view.View;
+
+import com.google.gson.JsonObject;
+import com.squareup.picasso.Picasso;
+
+import java.util.Date;
+import java.util.List;
+
+import closer.vlllage.com.closer.R;
+import closer.vlllage.com.closer.handler.event.EventDetailsHandler;
+import closer.vlllage.com.closer.handler.helpers.ApplicationHandler;
+import closer.vlllage.com.closer.handler.helpers.JsonHandler;
+import closer.vlllage.com.closer.handler.helpers.ResourcesHandler;
+import closer.vlllage.com.closer.handler.helpers.Val;
+import closer.vlllage.com.closer.handler.phone.NameHandler;
+import closer.vlllage.com.closer.handler.phone.PhoneMessagesHandler;
+import closer.vlllage.com.closer.pool.PoolMember;
+import closer.vlllage.com.closer.store.StoreHandler;
+import closer.vlllage.com.closer.store.models.Event;
+import closer.vlllage.com.closer.store.models.GroupMessage;
+import closer.vlllage.com.closer.store.models.Phone;
+import closer.vlllage.com.closer.store.models.Phone_;
+import closer.vlllage.com.closer.store.models.Suggestion;
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
+
+import static android.text.format.DateUtils.MINUTE_IN_MILLIS;
+import static android.text.format.DateUtils.WEEK_IN_MILLIS;
+
+public class MessageDisplay extends PoolMember {
+
+    public void displayAction(GroupMessagesAdapter.GroupMessageViewHolder holder, JsonObject jsonObject, GroupMessage groupMessage) {
+        Phone phone = getPhone(groupMessage.getFrom());
+        String contactName = $(NameHandler.class).getName(phone);
+
+        JsonObject action = jsonObject.get("action").getAsJsonObject();
+        holder.name.setVisibility(View.VISIBLE);
+        holder.name.setText(contactName + " " + action.get("intent").getAsString());
+        holder.time.setVisibility(View.VISIBLE);
+        holder.time.setText(getTimeString(groupMessage.getTime()));
+
+        String comment = action.get("comment").getAsString();
+        if ($(Val.class).isEmpty(comment)) {
+            holder.message.setVisibility(View.GONE);
+        } else {
+            holder.message.setVisibility(View.VISIBLE);
+            holder.message.setGravity(Gravity.START);
+            holder.message.setAlpha(1f);
+            holder.message.setText(action.get("comment").getAsString());
+        }
+        holder.action.setVisibility(View.GONE);
+
+        holder.action.setVisibility(View.VISIBLE);
+        holder.action.setText($(ResourcesHandler.class).getResources().getString(R.string.reply));
+        holder.action.setOnClickListener(view -> $(PhoneMessagesHandler.class).openMessagesWithPhone(phone.getId(), contactName, comment));
+    }
+
+    public void displayGroupMessage(GroupMessagesAdapter.GroupMessageViewHolder holder, boolean isReversed, GroupMessage groupMessage, List<GroupMessage> groupMessages, int position, int itemCount) {
+        int pairIndex = position + (isReversed ? -1 : 1);
+        boolean validIndex = pairIndex >= 0 && pairIndex < itemCount;
+
+        boolean previousMessageIsSameContact = validIndex &&
+                groupMessages.get(pairIndex).getAttachment() == null &&
+                groupMessages.get(pairIndex).getFrom() != null &&
+                groupMessages.get(pairIndex).getFrom().equals(groupMessage.getFrom());
+
+        boolean previousMessageIsSameGroup = validIndex &&
+                groupMessages.get(pairIndex).getTo() != null &&
+                groupMessages.get(pairIndex).getTo().equals(groupMessage.getTo());
+
+        boolean previousMessageIsSameTime = validIndex &&
+                groupMessages.get(pairIndex).getAttachment() == null &&
+                getTimeString(groupMessages.get(pairIndex).getTime()).equals(getTimeString(groupMessage.getTime()));
+
+        holder.action.setVisibility(View.GONE);
+        holder.name.setVisibility(previousMessageIsSameContact && previousMessageIsSameGroup ? View.GONE : View.VISIBLE);
+        holder.message.setVisibility(View.VISIBLE);
+        holder.message.setGravity(Gravity.START);
+
+        if (previousMessageIsSameTime) {
+            holder.time.setVisibility(View.GONE);
+        } else {
+            holder.time.setVisibility(View.VISIBLE);
+            holder.time.setText(getTimeString(groupMessage.getTime()));
+        }
+
+        Phone phone = null;
+
+        if (groupMessage.getFrom() != null) {
+            phone = getPhone(groupMessage.getFrom());
+        }
+
+        holder.name.setText($(NameHandler.class).getName(phone));
+        holder.message.setText(groupMessage.getText());
+        holder.message.setAlpha(groupMessage.isLocalOnly() ? .5f : 1f);
+    }
+
+    public void displayMessage(GroupMessagesAdapter.GroupMessageViewHolder holder, JsonObject jsonObject, GroupMessage groupMessage) {
+        holder.name.setVisibility(View.GONE);
+        holder.time.setVisibility(View.GONE);
+        holder.message.setVisibility(View.VISIBLE);
+        holder.message.setGravity(Gravity.CENTER_HORIZONTAL);
+        holder.message.setText(jsonObject.get("message").getAsString());
+        holder.message.setAlpha(.5f);
+        holder.itemView.setOnClickListener(null);
+        holder.action.setVisibility(View.GONE);
+    }
+
+    public void displayEvent(GroupMessagesAdapter.GroupMessageViewHolder holder, JsonObject jsonObject, GroupMessage groupMessage, GroupMessagesAdapter.OnEventClickListener onEventClickListener) {
+        final Event event = $(JsonHandler.class).from(jsonObject.get("event"), Event.class);
+
+        holder.name.setVisibility(View.VISIBLE);
+        holder.time.setVisibility(View.VISIBLE);
+        holder.time.setText(getTimeString(groupMessage.getTime()));
+
+        Phone phone = getPhone(groupMessage.getFrom());
+
+        String contactName = $(NameHandler.class).getName(phone);
+
+        holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.phone_shared_an_event, contactName));
+
+        holder.message.setVisibility(View.VISIBLE);
+        holder.message.setGravity(Gravity.START);
+        holder.message.setAlpha(1f);
+        holder.message.setText(
+                (event.getName() == null ? $(ResourcesHandler.class).getResources().getString(R.string.unknown) : event.getName()) +
+                        "\n" +
+                        $(EventDetailsHandler.class).formatEventDetails(event));
+
+        holder.action.setVisibility(View.VISIBLE);
+        holder.action.setText($(ResourcesHandler.class).getResources().getString(R.string.show_on_map));
+        holder.action.setOnClickListener(view -> {
+            if (onEventClickListener != null) {
+                onEventClickListener.onEventClick(event);
+            }
+        });
+    }
+
+    public void displaySuggestion(GroupMessagesAdapter.GroupMessageViewHolder holder, JsonObject jsonObject, GroupMessage groupMessage, GroupMessagesAdapter.OnSuggestionClickListener onSuggestionClickListener) {
+        final Suggestion suggestion = $(JsonHandler.class).from(jsonObject.get("suggestion"), Suggestion.class);
+
+        boolean suggestionHasNoName = suggestion == null || suggestion.getName() == null || suggestion.getName().isEmpty();
+
+        holder.name.setVisibility(View.VISIBLE);
+        holder.time.setVisibility(View.VISIBLE);
+        holder.time.setText(getTimeString(groupMessage.getTime()));
+
+        Phone phone = getPhone(groupMessage.getFrom());
+
+        String contactName = $(NameHandler.class).getName(phone);
+
+        if (suggestionHasNoName) {
+            holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.phone_shared_a_location, contactName));
+        } else {
+            holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.phone_shared_a_suggestion, contactName));
+        }
+
+        if (suggestionHasNoName) {
+            holder.message.setVisibility(View.GONE);
+        } else {
+            holder.message.setVisibility(View.VISIBLE);
+            holder.message.setGravity(Gravity.START);
+            holder.message.setAlpha(1f);
+            holder.message.setText(suggestion.getName());
+        }
+
+        holder.action.setVisibility(View.VISIBLE);
+        holder.action.setText($(ResourcesHandler.class).getResources().getString(R.string.show_on_map));
+        holder.action.setOnClickListener(view -> {
+            if (onSuggestionClickListener != null) {
+                onSuggestionClickListener.onSuggestionClick(suggestion);
+            }
+        });
+    }
+
+    public void displayPhoto(GroupMessagesAdapter.GroupMessageViewHolder holder, JsonObject jsonObject, GroupMessage groupMessage) {
+        Phone phone = getPhone(groupMessage.getFrom());
+        String contactName = $(NameHandler.class).getName(phone);
+
+        final String photo = jsonObject.get("photo").getAsString() + "?s=500";
+        holder.name.setVisibility(View.VISIBLE);
+        holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.phone_shared_a_photo, contactName));
+        holder.time.setVisibility(View.VISIBLE);
+        holder.time.setText(getTimeString(groupMessage.getTime()));
+        holder.message.setVisibility(View.GONE);
+        holder.action.setVisibility(View.GONE);// or Share / save photo?
+        holder.photo.setVisibility(View.VISIBLE);
+        holder.photo.setOnClickListener(view -> $(PhotoActivityTransitionHandler.class).show(view, photo));
+        Picasso.get().cancelRequest(holder.photo);
+        Picasso.get().load(photo).transform(new RoundedCornersTransformation($(ResourcesHandler.class).getResources().getDimensionPixelSize(R.dimen.imageCorners), 0)).into(holder.photo);
+    }
+
+    public void displayFallback(GroupMessagesAdapter.GroupMessageViewHolder holder, GroupMessage groupMessage) {
+        holder.name.setText($(ResourcesHandler.class).getResources().getString(R.string.unknown));
+        holder.message.setText("");
+        holder.time.setVisibility(View.VISIBLE);
+        holder.time.setText(getTimeString(groupMessage.getTime()));
+    }
+
+    private Phone getPhone(String phoneId) {
+        if (phoneId == null) {
+            return null;
+        }
+
+        return $(StoreHandler.class).getStore().box(Phone.class).query()
+                .equal(Phone_.id, phoneId)
+                .build()
+                .findFirst();
+    }
+
+    private String getTimeString(Date date) {
+        if (date == null) {
+            return "";
+        }
+
+        if (new Date().getTime() - date.getTime() < 5 * MINUTE_IN_MILLIS) {
+            return "";
+        }
+
+        if (DateUtils.isToday(date.getTime())) {
+            return DateUtils.getRelativeTimeSpanString(date.getTime()).toString();
+        }
+
+        return DateUtils.getRelativeDateTimeString(
+                $(ApplicationHandler.class).getApp(),
+                date.getTime(),
+                MINUTE_IN_MILLIS,
+                WEEK_IN_MILLIS,
+                0
+        ).toString();
+    }
+}
