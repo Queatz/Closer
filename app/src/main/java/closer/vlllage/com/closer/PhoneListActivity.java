55d934c8b7a81d8034bb3a0a0fd0e1ca2534c784
Jacob Ferrero <jacobaferrero@gmail.com>
2019-01-12 13:46:27 -0600

    Remove heart

--- app/src/main/java/closer/vlllage/com/closer/PhoneListActivity.java
+++ app/src/main/java/closer/vlllage/com/closer/PhoneListActivity.java
@@ -9,8 +9,13 @@ import com.google.android.gms.maps.model.LatLng;
 import closer.vlllage.com.closer.api.models.PhoneResult;
 import closer.vlllage.com.closer.handler.data.AccountHandler;
 import closer.vlllage.com.closer.handler.data.ApiHandler;
+import closer.vlllage.com.closer.handler.data.PersistenceHandler;
+import closer.vlllage.com.closer.handler.data.RefreshHandler;
+import closer.vlllage.com.closer.handler.helpers.ApplicationHandler;
 import closer.vlllage.com.closer.handler.helpers.DefaultAlerts;
 import closer.vlllage.com.closer.handler.helpers.DisposableHandler;
+import closer.vlllage.com.closer.handler.helpers.ResourcesHandler;
+import closer.vlllage.com.closer.handler.helpers.ToastHandler;
 import closer.vlllage.com.closer.handler.map.MapActivityHandler;
 import closer.vlllage.com.closer.handler.phone.NameHandler;
 import closer.vlllage.com.closer.handler.phone.PhoneAdapter;
@@ -28,6 +33,23 @@ public class PhoneListActivity extends ListActivity {
         $(ApiHandler.class).setAuthorization($(AccountHandler.class).getPhone());
 
         PhoneAdapter adapter = new PhoneAdapterHeaderAdapter($(PoolMember.class), reactionResult -> {
+            if (reactionResult.from.equals($(PersistenceHandler.class).getPhoneId())) {
+                $(DefaultAlerts.class).message($(ResourcesHandler.class).getResources().getString(R.string.remove_heart), result -> {
+                    $(DisposableHandler.class).add($(ApiHandler.class).reactToMessage(reactionResult.to, "♥", true)
+                            .subscribe(successResult -> {
+                                if (successResult.success) {
+                                    $(ToastHandler.class).show(R.string.heart_removed);
+                                    $(ApplicationHandler.class).getApp().$(RefreshHandler.class).refreshGroupMessage(reactionResult.to);
+                                    finish();
+                                } else {
+                                    $(DefaultAlerts.class).thatDidntWork();
+                                }
+                            }, error -> $(DefaultAlerts.class).thatDidntWork()));
+                });
+
+                return;
+            }
+
             finish(() -> {
                 $(MapActivityHandler.class).replyToPhone(
                         reactionResult.phone.id,
