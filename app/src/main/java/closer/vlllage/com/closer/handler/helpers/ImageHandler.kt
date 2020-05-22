3cf00de8eea380eeee66e2195d6ce93ca520bdae
Jacob Ferrero <jacobaferrero@gmail.com>
2020-05-22 00:32:57 -0500

    Fix for low res images on map

--- app/src/main/java/closer/vlllage/com/closer/handler/helpers/ImageHandler.kt
+++ app/src/main/java/closer/vlllage/com/closer/handler/helpers/ImageHandler.kt
@@ -2,8 +2,8 @@ package closer.vlllage.com.closer.handler.helpers
 
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import com.queatz.on.On
 import com.queatz.on.OnLifecycle
 
@@ -11,7 +11,7 @@ class ImageHandler constructor(private val on: On) : OnLifecycle {
 
     override fun on() {
         if (glide == null)
-            glide = Glide.with(on<ApplicationHandler>().app).setDefaultRequestOptions(RequestOptions().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC))
+            glide = Glide.with(on<ApplicationHandler>().app).setDefaultRequestOptions(RequestOptions().override(SIZE_ORIGINAL))
     }
 
     fun get() = glide!!
